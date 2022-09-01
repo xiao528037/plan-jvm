880399756bc833987e6d4a4c647ec1c94772ae58
xiao-mac <543033627@qq.com>
2022-09-01 21:42:50 +0800

    Initial commit

---
+++ src/main/java/com/xiao/plainjvm/PlainJvmApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.plainjvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlainJvmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiao/plainjvm/PlainJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.plainjvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlainJvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
